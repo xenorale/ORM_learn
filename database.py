8b6942ef936751fe18b16e22be59fcf150733d4a
Vladislav <vlav2005@gmail.com>
2025-12-15 01:11:38 +0300

    1 и 2 задания

---
+++ database.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+DB_URL = "postgresql+psycopg://postgres:1234@localhost:5432/mydatabase"
+
+engine = create_engine(DB_URL, echo=True)
+session = Session(engine)
---
+++ models.py
@@ -0,0 +1,41 @@
+import sys
+from sqlalchemy import String, Float, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from database import engine
+
+sys.stdout.reconfigure(encoding='utf-8')
+
+class Base(DeclarativeBase):
+    pass
+
+class Warehouse(Base):
+    __tablename__ = 'Warehouses'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    address: Mapped[str]
+    capacity: Mapped[int]
+    items: Mapped[list["Item"]] = relationship(back_populates="warehouse")
+    def __repr__(self):
+        return f'Warehouse(id={self.id}, name={self.name}, address={self.address}, capacity={self.capacity})'
+
+class Item(Base):
+    __tablename__ = 'Items'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    price: Mapped[float] = mapped_column(Float)
+    warehouse_id: Mapped[int] = mapped_column(ForeignKey("Warehouses.id"))
+    warehouse: Mapped["Warehouse"] = relationship(back_populates="items")
+
+    def __repr__(self):
+        return f'Item(id={self.id}, name={self.name}, price={self.price})'
+
+class Order(Base):
+    __tablename__ = 'Orders'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    amount: Mapped[float] = mapped_column(default=0)
+    item_id: Mapped[int] = mapped_column(ForeignKey("Items.id"))
+    item: Mapped["Item"] = relationship()
+    def __repr__(self):
+        return f'Order(id={self.id}, amount={self.amount})'
+
+Base.metadata.create_all(engine)
